22c6cd6ac599a743f6a523ef97e4aa2a7b479072
kolhjuli <kolhjuli@kkg-do.eu>
2022-08-30 11:22:44 +0200

    HA 5 Propertys

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -3,7 +3,16 @@ public class MainProgram {
         Property p1 = new Property("Address: Erzbergerstraße 1",10,20.5);
         System.out.println("Address: " + p1.getAddress() + "\nSize: " + p1.getSize() + " m²");
 
-        Property p2 = new Property("Address: Erzbergerstraße 2",20,41);
+        Property p2 = new Property("Address: Erzbergerstraße 2",10,20.5);
         System.out.println("Address: " + p2.getAddress() + "\nSize: " + p2.getSize() + " m²");
+
+        Property p3 = new Property("Address: Erzbergerstraße 3",10,20.5);
+        System.out.println("Address: " + p3.getAddress() + "\nSize: " + p3.getSize() + " m²");
+
+        Property p4 = new Property("Address: Erzbergerstraße 4",10,20.5);
+        System.out.println("Address: " + p4.getAddress() + "\nSize: " + p4.getSize() + " m²");
+
+        Property p5 = new Property("Address: Erzbergerstraße 5",10,20.5);
+        System.out.println("Address: " + p5.getAddress() + "\nSize: " + p5.getSize() + " m²");
     }
 }
