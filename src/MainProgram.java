8955ff9a8a9779fb355bb827c12640b997a4cfa0
kolhjuli <kolhjuli@kkg-do.eu>
2022-08-30 11:12:11 +0200

    P2

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,7 +1,9 @@
 public class MainProgram {
     public static void main(String[] args) {
-        Property p1 = new Property();
-
+        Property p1 = new Property("Address: Erzbergerstraße 2",10,20.5);
         System.out.println("Address: " + p1.getAddress() + "\nSize: " + p1.getSize() + " m²");
+
+        Property p2 = new Property("Address: Erzbergerstraße 2",20,41);
+        System.out.println("Address: " + p2.getAddress() + "\nSize: " + p2.getSize() + " m²");
     }
 }
--- src/Property.java
+++ src/Property.java
@@ -3,10 +3,10 @@ public class Property {
     public double width;
     private double length;
 
-    public Property(){
-        address = "Erzbergerstraße 1";
-        width = 10;
-        length = 20.5;
+    public Property(String a,double w,double l){
+        address = a;
+        width = w;
+        length = l;
     }
 
     public String getAddress(){
