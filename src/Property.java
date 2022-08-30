6476bab15266f7f87cc2b66316f85d9616849de7
kolhjuli <kolhjuli@kkg-do.eu>
2022-08-30 10:39:10 +0200

    Initial commit

---
+++ src/Property.java
@@ -0,0 +1,12 @@
+public class Property {
+    private String address;
+    private double width;
+    private double length;
+
+    public Property(){
+        address = "Erzbergerstraße 1";
+        width = 10;
+        length = 20.5;
+    }
+
+}
