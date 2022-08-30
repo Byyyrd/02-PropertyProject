d86f4e3331e9dc172f7a4e23cf0a3f6a1814f2ab
kolhjuli <kolhjuli@kkg-do.eu>
2022-08-30 11:15:10 +0200

    rICHTIGE aDRESSE

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,6 +1,6 @@
 public class MainProgram {
     public static void main(String[] args) {
-        Property p1 = new Property("Address: Erzbergerstraße 2",10,20.5);
+        Property p1 = new Property("Address: Erzbergerstraße 1",10,20.5);
         System.out.println("Address: " + p1.getAddress() + "\nSize: " + p1.getSize() + " m²");
 
         Property p2 = new Property("Address: Erzbergerstraße 2",20,41);
